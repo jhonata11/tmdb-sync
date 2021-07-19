4328eef3c0daf02be418b83aefb9dd537af3503a
Jhonata da Rocha <jhonata11@hotmail.com>
2021-07-20 01:16:46 +0200

    add command line to export

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,22 @@ import { users } from './config'
 import * as importer from './importer'
 import * as exporter from './exporter'
 
-async function start(users: string[]) {
+enum RunMode {
+  IMPORT,
+  EXPORT
+}
+
+async function start(users: string[], mode: RunMode) {
   for (const user of users) {
-    await importer.importMovies(user)
-    // await exporter.exportMovies(user)
+    if(runMode === RunMode.EXPORT) {
+      await exporter.exportMovies(user)
+    }
+    if(runMode === RunMode.IMPORT) {
+      await importer.importMovies(user)
+    }
   }
 }
 
-start(users)
+const [exe, file, arg] = process.argv
+const runMode = arg === '-e' ? RunMode.EXPORT : RunMode.IMPORT
+start(users, runMode)
